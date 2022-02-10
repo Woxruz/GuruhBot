b253354bf53d21a4aa84843977ab07c347a69222
Shohruzbek Sultonov <74559850+Woxruz@users.noreply.github.com>
2022-02-10 15:41:13 +0300

    Initialize kanal-bot

---
+++ data/config.py
@@ -0,0 +1,10 @@
+from environs import Env
+
+# environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
+
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = "1758998080:AAEAh0BnT71WeNcxJvcLXM1Xu5REa36kKI0"  # Bot token
+ADMINS = ["1343692719",]  # adminlar ro'yxati
+CHANNELS = ["-1001572570088",]
---
+++ handlers/users/__init__.py
@@ -0,0 +1,5 @@
+from . import help
+from . import start
+from . import newpost
+from . import confirm
+# from . import echo
